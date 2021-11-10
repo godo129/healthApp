42a8c7e0759bf72852db3ea84f643e89229896c5
godo <godo129@naver.com>
2021-11-10 23:25:09 +0900

    사이드 메뉴에 HealthKit 이용해서 얻은 걸음 정보 뿌려주기

---
+++ healthApp/Data/HealthKitFunctions.swift
@@ -0,0 +1,12 @@
+//
+//  HealthKitFunctions.swift
+//  healthApp
+//
+//  Created by hong on 2021/11/10.
+//
+
+import UIKit
+import HealthKit
+
+
+
--- healthApp/SideMenuViewController.swift
+++ healthApp/SideMenuViewController.swift
@@ -8,9 +8,20 @@
 import UIKit
 import FirebaseStorage
 import Lottie
+import HealthKit
 
 
 class SideMenuViewController: UIViewController {
+    
+    var stepsLabel: UILabel = {
+        var stepsLabel = UILabel()
+        stepsLabel.textColor = .white
+        stepsLabel.textAlignment = .center
+        return stepsLabel
+    }()
+
+    
+    var healthStore = HKHealthStore()
     
     /*
     private var profileImageView: UIButton = {
@@ -42,6 +53,9 @@ class SideMenuViewController: UIViewController {
         return nickLabel
     }()
     
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -49,6 +63,14 @@ class SideMenuViewController: UIViewController {
         //view.addSubview(profileImageView)
         view.addSubview(nickLabel)
         nickLabel.text = nick
+        
+        view.addSubview(stepsLabel)
+        
+        stepsLabel.text = "\(UserDefaults.standard.value(forKey: "steps")!) 걸음"
+        
+        
+
+        
 
         
 /*
@@ -114,8 +136,13 @@ class SideMenuViewController: UIViewController {
         
         view.addSubview(dogAnimation)
         dogAnimation.play()
+        
+        
+        healthAuth(Year: 2021, Month: 11, Date: 10)
 
         }
+    
+    
         
         /*
         
@@ -172,8 +199,71 @@ class SideMenuViewController: UIViewController {
        // profileImageView.frame = CGRect(x: 20, y: 200, width: 260, height: 200)
         nickLabel.frame = CGRect(x: 100, y: profileImageView.frame.origin.y+200, width: 100, height: 50)
         
+        stepsLabel.frame = CGRect(x: 100, y: nickLabel.frame.origin.y+60, width: 100, height: 50)
+        
         dogAnimation.frame = CGRect(x: 0, y: nickLabel.frame.origin.y + 70, width: view.frame.size.width, height: view.frame.size.width)
     }
+    
+    
+    
+    func healthAuth(Year:Int, Month: Int, Date: Int) {
+            let share = Set([HKCategoryType.quantityType(forIdentifier: .stepCount)!])
+            let read = Set([HKCategoryType.quantityType(forIdentifier: .stepCount)!])
+            
+            healthStore.requestAuthorization(toShare: share, read: read) { (check,error) in
+                guard error == nil else {
+                    return
+                }
+                
+                if check {
+                    self.getSteps(Year: Year, Month: Month, Date: Date)
+                    
+                                        
+                }
+            }
+        }
+        
+        func getSteps(Year: Int, Month: Int, Date: Int){
+            
+            guard let sampleType = HKCategoryType.quantityType(forIdentifier: .stepCount) else {return }
+            
+            let dateformatter = DateFormatter()
+            dateformatter.dateFormat = "yyyy년MM월dd일 HH시mm분ss초 ZZZ"
+            let DateStirng1 = "\(Year)년\(Month)월\(Date)일 00시00분00초 +0000"
+            let DateStirng2 = "\(Year)년\(Month)월\(Date)일 23시59분59초 +0000"
+            let endDate = dateformatter.date(from: DateStirng2)!
+            let startDate = dateformatter.date(from: DateStirng1)!
+            //let startDate = Calendar.current.startOfDay(for: endDate)
+            
+            let predicate = HKQuery.predicateForSamples(withStart: startDate, end: endDate, options: .strictEndDate)
+            var interval = DateComponents()
+            interval.day = 1
+            
+            let query = HKStatisticsCollectionQuery(quantityType: sampleType, quantitySamplePredicate: predicate, options: [.cumulativeSum], anchorDate: startDate, intervalComponents: interval)
+            
+            query.initialResultsHandler = {
+                
+                query, result , error in
+                
+                if let myresult = result {
+                    myresult.enumerateStatistics(from: startDate, to: endDate) { (statistics, value) in
+                        
+                        if let count = statistics.sumQuantity() {
+                            let val = count.doubleValue(for: HKUnit.count())
+                            
+                            UserDefaults.standard.setValue(Int(val), forKey: "steps")
+                            
+                        } else {
+                            UserDefaults.standard.setValue(0, forKey: "steps")
+                        }
+                    }
+                }
+            }
+            healthStore.execute(query)
+            
+            
+        }
+
     
 
 }
